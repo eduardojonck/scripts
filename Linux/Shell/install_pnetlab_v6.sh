180ba85b64c5f3633edbf664d9eb709cef72ebee
Eduardo Jonck <eduardo@eduardojonck.com>
2025-07-21 20:26:48 -0300

    Update install_pnetlab_v6.sh
    Change repo PHP7.2 Ubuntu Bionic

--- Linux/Shell/install_pnetlab_v6.sh
+++ Linux/Shell/install_pnetlab_v6.sh
@@ -56,7 +56,7 @@ systemctl restart ssh &>/dev/null
 
 #install  packages required
 add-apt-repository --yes ppa:ondrej/php &>/dev/null
-echo "deb [trusted=yes] https://xuepudong.github.io/pnetlabv6lib/ focal main" | sudo tee /etc/apt/sources.list.d/pnetlab.list
+echo "deb [trusted=yes] http://archive.ubuntu.com/ubuntu bionic main universe" | sudo tee /etc/apt/sources.list.d/pnetlab.list
 apt-get update
 
 
@@ -82,17 +82,14 @@ fi
 rm /var/lib/dpkg/lock* &>/dev/null
 echo -e "${GREEN}Downloading dependencies for PNETLAB ${NO_COLOR}"
 #Deps
-apt-get install -y ifupdown unzip &>/dev/null
-apt-get install -y resolvconf libnet-pcap-perl duc libspice-client-glib-2.0-8 libtinfo5 libncurses5 libncursesw5 ntpdate vim dos2unix apache2 bridge-utils build-essential cpulimit debconf-utils dialog dmidecode genisoimage iptables lib32gcc1 lib32z1 pastebinit libc6 libc6-i386 libelf1 libpcap0.8 libsdl1.2debian logrotate lsb-release lvm2 ntp rsync sshpass autossh plymouth-label python3-pexpect sqlite3 tcpdump telnet uml-utilities zip libguestfs-tools cgroup-tools libyaml-0-2 net-tools python2 mysql-server libavcodec58 libavformat58 libavutil56 libswscale5 libfreerdp-client2-2 libfreerdp-server2-2 libfreerdp-shadow-subsystem2-2 libfreerdp-shadow2-2 libfreerdp2-2 winpr-utils gir1.2-pango-1.0 libpango-1.0-0 libpangocairo-1.0-0 libpangoft2-1.0-0 libpangoxft-1.0-0 pango1.0-tools pkg-config libssh2-1 libtelnet2 libvncclient1 libvncserver1 libwebsockets15 libpulse0 libpulse-mainloop-glib0 libssl1.1 libvorbis0a libvorbisenc2 libvorbisfile3 libwebp6 libwebpmux3 libwebpdemux2 libcairo2 libcairo-gobject2 libcairo-script-interpreter2 libjpeg62 libpng16-16 libtool libuuid1 libossp-uuid16 default-jdk default-jdk-headless rsync sshpass telnet tomcat9 tomcat9-admin tomcat9-docs libaio1 libasound2 libbrlapi0.7 libcacard0 libepoxy0 libfdt1 libgbm1 libgcc-s1 libglib2.0-0 libgnutls30 libibverbs1 libjpeg8 libncursesw6 libnettle7 libnuma1 libpixman-1-0 libpmem1 librdmacm1 libsasl2-2 libseccomp2 libslirp0 libspice-server1 libtinfo6 libusb-1.0-0 libusbredirparser1 libvirglrenderer1 zlib1g qemu-system-common qemu-system-data ipxe-qemu-256k-compat-efi-roms seabios ipxe-qemu udhcpd busybox libsdl2-2.0-0 libxenmisc4.11 libcapstone3 libvdeplug2 libnfs13 ovmf
+apt-get install -y ifupdown unzip resolvconf libnet-pcap-perl duc libspice-client-glib-2.0-8 libtinfo5 libncurses5 libncursesw5 ntpdate vim dos2unix apache2 bridge-utils build-essential cpulimit debconf-utils dialog dmidecode genisoimage iptables lib32gcc1 lib32z1 pastebinit libc6 libc6-i386 libelf1 libpcap0.8 libsdl1.2debian logrotate lsb-release lvm2 ntp rsync sshpass autossh plymouth-label python3-pexpect sqlite3 tcpdump telnet uml-utilities zip libguestfs-tools cgroup-tools libyaml-0-2 net-tools python2 mysql-server libavcodec58 libavformat58 libavutil56 libswscale5 libfreerdp-client2-2 libfreerdp-server2-2 libfreerdp-shadow-subsystem2-2 libfreerdp-shadow2-2 libfreerdp2-2 winpr-utils gir1.2-pango-1.0 libpango-1.0-0 libpangocairo-1.0-0 libpangoft2-1.0-0 libpangoxft-1.0-0 pango1.0-tools pkg-config libssh2-1 libtelnet2 libvncclient1 libvncserver1 libwebsockets15 libpulse0 libpulse-mainloop-glib0 libssl1.1 libvorbis0a libvorbisenc2 libvorbisfile3 libwebp6 libwebpmux3 libwebpdemux2 libcairo2 libcairo-gobject2 libcairo-script-interpreter2 libjpeg62 libpng16-16 libtool libuuid1 libossp-uuid16 default-jdk default-jdk-headless rsync sshpass telnet tomcat9 tomcat9-admin tomcat9-docs libaio1 libasound2 libbrlapi0.7 libcacard0 libepoxy0 libfdt1 libgbm1 libgcc-s1 libglib2.0-0 libgnutls30 libibverbs1 libjpeg8 libncursesw6 libnettle7 libnuma1 libpixman-1-0 libpmem1 librdmacm1 libsasl2-2 libseccomp2 libslirp0 libspice-server1 libtinfo6 libusb-1.0-0 libusbredirparser1 libvirglrenderer1 zlib1g qemu-system-common qemu-system-data ipxe-qemu-256k-compat-efi-roms seabios ipxe-qemu udhcpd busybox libsdl2-2.0-0 libxenmisc4.11 libcapstone3 libvdeplug2 libnfs13 ovmf
 # PHP
-apt-get install -y php-common php7.2 php7.2-cli php7.2-common php7.2-curl php7.2-gd php7.2-imagick php7.2-json php7.2-mbstring php7.2-mysql php7.2-opcache php7.2-readline php7.2-sqlite3 php7.2-xml php7.2-zip libapache2-mod-php7.2
-
+apt-get install -y php-common php7.2 php7.2-cli php7.2-common php7.2-curl php7.2-gd php-imagick php7.2-json php7.2-mbstring php7.2-mysql php7.2-opcache php7.2-readline php7.2-sqlite3 php7.2-xml php7.2-zip libapache2-mod-php7.2
 #Set php7.2 default
 update-alternatives --set php /usr/bin/php7.2 &>/dev/null
 
 echo -e "${GREEN}Downloading PNETLAB PACKAGES ...${NO_COLOR}"
 cd /tmp
-
 echo -e "${GREEN}$DOwnload Packages${NO_COLOR}"
 dpkg-query -l | grep linux-image-5.17.15-pnetlab-uksm-2 | grep 5.17.15-pnetlab-uksm-2-1 -q
 if [ $? -ne 0 ]; then
@@ -187,7 +184,6 @@ azure_kernel_tune() {
 dmidecode -t bios | grep -q Google && gcp_tune
 
 # Azure
-
 uname -a | grep -q -- "-azure " && azure_kernel_tune
 
 apt autoremove -y -q
