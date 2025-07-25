812398b39b866c59bfa07f957ef2a8d25d43c871
Eduardo Jonck <eduardo@eduardojonck.com>
2025-07-25 10:22:18 -0300

    Update install_pnetlab_v6_pt_br.sh

--- Linux/Shell/PnetLab/install_pnetlab_v6_pt_br.sh
+++ Linux/Shell/PnetLab/install_pnetlab_v6_pt_br.sh
@@ -22,8 +22,6 @@ fi
 
 log_info "Atualizando repositórios e instalando pacotes base necessários..."
 apt-get update -qq
-
-log_info "Instalando pacotes base complementares..."
 apt-get install -y  ifupdown unzip resolvconf wget libnet-pcap-perl duc libspice-client-glib-2.0-8 libtinfo5 libncurses5 libncursesw5 ntpdate vim dos2unix apache2 bridge-utils build-essential cpulimit debconf-utils dialog dmidecode genisoimage iptables lib32gcc1 lib32z1 pastebinit libc6 libc6-i386 libelf1 libpcap0.8 libsdl1.2debian logrotate lsb-release lvm2 ntp rsync sshpass autossh plymouth-label python3-pexpect sqlite3 tcpdump telnet uml-utilities zip libguestfs-tools cgroup-tools libyaml-0-2 net-tools python2 mysql-server libavcodec58 libavformat58 libavutil56 libswscale5 libfreerdp-client2-2 libfreerdp-server2-2 libfreerdp-shadow-subsystem2-2 libfreerdp-shadow2-2 libfreerdp2-2 winpr-utils gir1.2-pango-1.0 libpango-1.0-0 libpangocairo-1.0-0 libpangoft2-1.0-0 libpangoxft-1.0-0 pango1.0-tools pkg-config libssh2-1 libtelnet2 libvncclient1 libvncserver1 libwebsockets15 libpulse0 libpulse-mainloop-glib0 libssl1.1 libvorbis0a libvorbisenc2 libvorbisfile3 libwebp6 libwebpmux3 libwebpdemux2 libcairo2 libcairo-gobject2 libcairo-script-interpreter2 libjpeg62 libpng16-16 libtool libuuid1 libossp-uuid16 default-jdk default-jdk-headless rsync sshpass telnet tomcat9 tomcat9-admin tomcat9-docs libaio1 libasound2 libbrlapi0.7 libcacard0 libepoxy0 libfdt1 libgbm1 libgcc-s1 libglib2.0-0 libgnutls30 libibverbs1 libjpeg8 libncursesw6 libnettle7 libnuma1 libpixman-1-0 libpmem1 librdmacm1 libsasl2-2 libseccomp2 libslirp0 libspice-server1 libtinfo6 libusb-1.0-0 libusbredirparser1 libvirglrenderer1 zlib1g qemu-system-common qemu-system-data ipxe-qemu-256k-compat-efi-roms seabios ipxe-qemu udhcpd busybox libsdl2-2.0-0 libxenmisc4.11 libcapstone3 libvdeplug2 libnfs13 ovmf
 
 
@@ -36,7 +34,6 @@ for cmd in wget unzip dpkg systemctl systemd-detect-virt lvextend resize2fs; do
 done
 
 KERNEL=pnetlab_kernel.zip
-
 URL_KERNEL=https://drive.labhub.eu.org/0:/upgrades_pnetlab/Focal/L/linux-5.17.15-pnetlab-uksm/pnetlab_kernel.zip
 URL_PRE_DOCKER=https://drive.labhub.eu.org/0:/upgrades_pnetlab/Focal/D/pre-docker.zip
 URL_PNET_GUACAMOLE=https://drive.labhub.eu.org/0:/upgrades_pnetlab/Focal/P/PNET_GUACAMOLE/pnetlab-guacamole_6.0.0-7_amd64.deb
@@ -59,11 +56,12 @@ log_info "Versão do Ubuntu: $UBUNTU_VERSION"
 rm -f /var/lib/dpkg/lock* /var/lib/apt/lists/lock /var/cache/apt/archives/lock
 dpkg --configure -a
 
+log_info "Habilitando acesso como root ao SSH...."
 sed -i -e "s/^#*PermitRootLogin .*/PermitRootLogin yes/" /etc/ssh/sshd_config
 sed -i -e "s/^#*DefaultTimeoutStopSec=.*/DefaultTimeoutStopSec=5s/" /etc/systemd/system.conf
 systemctl restart ssh
 
-log_info "Instalando PHP 7.2 para PNETLAB 6"
+log_info "Instalando PHP 7.2 para PNETLAB 6 do repositório de Eduardo Jonck <eduardo@eduardojonck.com>"
 echo "deb [trusted=yes] https://eduardojonck.com/repo/pnetlab/v6/php7.2 focal main" > /etc/apt/sources.list.d/eduardojonck.list
 apt-get update -qq
 apt-get install -y \
